718e269bad26e2c279738a9f914df916e4a040cf
virtual <amit4081596kushwaha@gmail.com>
2018-10-26 20:54:51 +0530

    all sorting function implementation are added

---
+++ mylib/mylib.h
@@ -0,0 +1,9 @@
+void swap(int *,int *);
+void display_array(int *,int);
+void bubble_sort(int *,int,int);
+void insertion_sort(int *,int,int);
+void selection_sort(int *,int,int);
+void quick_sort(int *,int,int,int);
+void merge_sort(int *,int,int,int);
+void build_heap(int *,int);
+void heap_sort(int *,int);
